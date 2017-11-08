07e3d74b668adb288912d28a42066f8ec8441429
Michael Yosowitz <michaelyosowitz@gmail.com>
2017-11-07 17:20:57 -0800

    second commit

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -1,58 +1,9 @@
-/***
- * Excerpted from "Rails, Angular, Postgres, and Bootstrap, Second Edition",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material,
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose.
- * Visit http://www.pragmaticprogrammer.com/titles/dcbang2 for more book information.
-***/
 import "hello_angular/polyfills";
 
 import { Component, NgModule    } from "@angular/core";
 import { BrowserModule          } from "@angular/platform-browser";
 import { FormsModule            } from "@angular/forms";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { Http,HttpModule        } from "@angular/http";
-
-var RESULTS = [
-  {
-    first_name: "Pat",
-    last_name: "Smith",
-    username: "psmith",
-    email: "pat.smith@somewhere.net",
-    created_at: "2016-02-05",
-  },
-  {
-    first_name: "Patrick",
-    last_name: "Jones",
-    username: "pjpj",
-    email: "jones.p@business.net",
-    created_at: "2014-03-05",
-  },
-  {
-    first_name: "Patricia",
-    last_name: "Benjamin",
-    username: "pattyb",
-    email: "benjie@aol.info",
-    created_at: "2016-01-02",
-  },
-  {
-    first_name: "Patty",
-    last_name: "Patrickson",
-    username: "ppat",
-    email: "pppp@freemail.computer",
-    created_at: "2016-02-05",
-  },
-  {
-    first_name: "Jane",
-    last_name: "Patrick",
-    username: "janesays",
-    email: "janep@company.net",
-    created_at: "2013-01-05",
-  },
-];
-
-
 
 var CustomerSearchComponent = Component({
   selector: "shine-customer-search",
@@ -62,67 +13,44 @@ var CustomerSearchComponent = Component({
 </header> \
 <section class="search-form"> \
   <form> \
-    <label for="keywords" class="sr-only">Keywords></label> \
-    <input type="text" id="keywords" name="keywords" \
-           placeholder="First Name, Last Name, or Email Address"\
-           bind-ngModel="keywords" \
-           on-ngModelChange="search($event)" \
-           class="form-control input-lg">\
+    <div class="input-group input-group-lg"> \
+      <label for="keywords" class="sr-only">Keywords></label> \
+      <input type="text" id="keywords" name="keywords" \
+             placeholder="First Name, Last Name, or Email Address"\
+             class="form-control input-lg">\
+      <span class="input-group-btn"> \
+        <input type="submit" value="Find Customers"\
+               class="btn btn-primary btn-lg">\
+      </span> \
+    </div> \
   </form> \
 </section> \
-<section class="search-results" *ngIf="customers"> \
+<section class="search-results"> \
   <header> \
     <h1 class="h3">Results</h1> \
   </header> \
   <ol class="list-group"> \
-    <li *ngFor="let customer of customers" \
-      class="list-group-item clearfix"> \
+    <li class="list-group-item clearfix"> \
       <h3 class="pull-right"> \
         <small class="text-uppercase">Joined</small> \
-        {{customer.created_at}} \
+        2016-01-01\
       </h3> \
       <h2 class="h3"> \
-        {{customer.first_name}} {{customer.last_name}} \
-        <small>{{customer.username}}</small> \
+        Pat Smith\
+        <small>psmith34</small> \
       </h2> \
-      <h4>{{customer.email}}</h4> \
+      <h4>pat.smith@example.com</h4> \
     </li> \
   </ol> \
 </section> \
   '
 }).Class({
-  constructor: [
-    Http,
-    function(http) {
-      this.customers = null;
-      this.http      = http;
-      this.keywords  = "";
-    }
-  ],
-  search: function($event) {
-    var self = this;
-    self.keywords = $event;
-    if (self.keywords.length < 3) {
-      return;
-    }
-    self.http.get(
-      "/customers.json?keywords=" + self.keywords
-    ).subscribe(
-      function(response) {
-        self.customers = response.json().customers;
-      },
-      function(response) {
-        window.alert(response);
-      }
-    );
+  constructor: function() {
   }
 });
 
 var CustomerAppModule = NgModule({
-  imports:      [
-    BrowserModule,
-    FormsModule,
-    HttpModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ CustomerSearchComponent ],
   bootstrap:    [ CustomerSearchComponent ]
 })
